d79afb581acf57f817c7e9489e84ea8611f4858b
Jose Uriel Contreras Arevalos <128828694+Urielcont@users.noreply.github.com>
2024-09-20 01:03:59 -0600

    Conexion bd

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { PORT } = require('./config.js')
 const rutas = require("./routes/rutas.js");
+const db = require('../src/config/configDB.js'); // Importa la configuración de la base de datos
 
 // servidor de express
 const app = express()
@@ -14,6 +15,17 @@ app.use('/api', rutas);
 app.get('/', (req, res) => {
     res.send('Servidor funcionando')
 })
+// Ejemplo de ruta usando la base de datos
+app.get('/api/usuarios', (req, res) => {
+    db.query('SELECT * FROM usuario', (err, results) => {
+        if (err) {
+            console.error('Error al ejecutar la consulta:', err.stack);
+            res.status(500).send('Error al obtener datos');
+            return;
+        }
+        res.json(results);
+    });
+});
 
 // Manejo de errores
 app.use((err, req, res, next) => {
