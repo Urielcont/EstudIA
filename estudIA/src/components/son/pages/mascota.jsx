7dc5b22b219a7c91273c2b85c9e82b1196a390b3
palkull <palkulx2000@gmail.com>
2024-09-20 06:30:09 -0600

    Mascota

---
+++ estudIA/src/components/son/pages/mascota.jsx
@@ -0,0 +1,10 @@
+export default function Mascota(){
+    return(
+        <>
+            <div className="Seleccionador">
+                <div></div>
+            </div>
+            
+        </>
+    )
+}
