d0edc91d522975a4058a7afe974dec16a7a34339
palkull <palkulx2000@gmail.com>
2024-09-20 06:14:29 -0600

    Materias

--- estudIA/src/components/son/pages/perfil.jsx
+++ estudIA/src/components/son/pages/perfil.jsx
@@ -1,12 +1,16 @@
-import '../css/perfil.css'
+import React from 'react';
+import '../css/perfil.css';
 
-export default function Perfilson(){
-    return <>
-    <div>
-        
-    </div>
-    <div className='fondo'>
-
-    </div>
-    </>
+export default function Perfilson() {
+    return (
+        <>
+            <div className='fondo'>
+                <div className='content'>
+                    <label htmlFor="">Color </label>
+                    <input type="color" name="" id="" />
+                </div>
+            </div>
+            <div className='imagen'></div>
+        </>
+    );
 }
